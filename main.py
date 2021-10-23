b63c38309780a0fd45f647b35e2251ef55f55709
JuanFe <55063644+JuanFeA98@users.noreply.github.com>
2021-10-23 18:37:32 -0500

    Validaciones

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 
 # FastAPI
 from fastapi import FastAPI
-from fastapi import Body, Query
+from fastapi import Body, Query, Path
 
 app = FastAPI()
 
@@ -19,14 +19,9 @@ class Person(BaseModel):
     is_married: Optional[bool] = None
 
 # Routes
-
 @app.get('/')
 def home():
     return {'message': "Hello, world!"}
-
-@app.get('/alternativa')
-def alt():
-    return {'message': "Este es otro endpoint"}
 
 @app.get('/alt/{id}')
 def alt_id(id):
@@ -37,11 +32,34 @@ def alt_id(id):
 def new_person(persona: Person = Body(...)):
     return persona
 
-# Validaciones
+# Validaciones query parameters
 @app.get('/person/detail')
 def show_person(
-    name: Optional[str] = Query(None, min_length=1, max_length=50), 
-    age: str = Query(...)
+    name: Optional[str] = Query(
+        None, 
+        min_length=1, 
+        max_length=50,
+        title='Person Name',
+        description='This is the person name'
+        ), 
+    age: int = Query(
+        ...,
+        title='Person Age',
+        description='This is the person age'
+        )
     ):
 
     return {name: age}
+
+# Validaciones path parameters
+@app.get('/person/detail/{person_id}')
+def show_person(
+    person_id: int = Path(
+        ..., 
+        gt=0,
+        title='Person Id',
+        description='This is the Person Id'
+        )
+    ):
+
+    return {person_id: 'Succesfull'}
